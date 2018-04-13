53c1a7d821a80b0f852662d25a843d57481f7453
fadedDexofan <fadedDexofan@protonmail.com>
2018-04-13 15:39:13 +1000

    fix logging

--- app/handlers/payments/checkout.js
+++ app/handlers/payments/checkout.js
@@ -1,5 +1,6 @@
 const { Event } = require("../../models");
 const { sendInvoice } = require("../../helpers");
+const { logger } = require("../../utils");
 
 const checkoutHandler = async (ctx) => {
   const eventName = ctx.update.callback_query.data;
@@ -7,6 +8,9 @@ const checkoutHandler = async (ctx) => {
   if (event) {
     await sendInvoice(ctx, event);
   } else {
+    logger.warn(
+      `${ctx.from.username} (${ctx.from.id}) попытался выставить счет за несуществующее событие`,
+    );
     ctx.reply("Не удалось выставить счет.");
   }
 };
