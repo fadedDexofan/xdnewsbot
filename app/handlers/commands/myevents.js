e96cf6d581fc52fea013d0e3f2424c96b21d87e7
fadedDexofan <fadedDexofan@protonmail.com>
2018-04-17 17:54:20 +1000

    сасат

--- app/handlers/commands/myevents.js
+++ app/handlers/commands/myevents.js
@@ -14,9 +14,9 @@ const myEventsHandler = async (ctx) => {
     );
     organizerEvents.concat("\nДля просмотра участников используйте команду `/show <id события>`");
   }
-  const eventsButtons = events.map((event) => [
-    Markup.callbackButton(event.name, `${event.id}_show`),
-  ]);
+  const eventsButtons = events
+    .map((event) => [Markup.callbackButton(event.name, `${event.id}_show`)])
+    .concat([Markup.callbackButton("Меню", "menu")]);
   ctx.replyWithMarkdown(organizerEvents, Markup.inlineKeyboard(eventsButtons).extra());
 };
 
